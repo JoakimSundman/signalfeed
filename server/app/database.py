d15ff778a5a3ea97a05397bfb652a68795c9a04d
Joakim Sundman <jsundma@gmail.com>
2026-08-06 18:02:53 +0200

    chore: added comment for deployment

--- server/app/database.py
+++ server/app/database.py
@@ -3,6 +3,7 @@ import os
 import sqlalchemy
 import sqlalchemy.orm
 
+# CHANGE echo=True to False when deploying
 engine = sqlalchemy.create_engine(os.environ["DATABASE_URL"], echo=True)
 
 session_creator = sqlalchemy.orm.sessionmaker(bind=engine)
