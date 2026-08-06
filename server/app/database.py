2e39e639493afe66e17e0030a67bd5fafdd6fe00
Joakim Sundman <jsundma@gmail.com>
2026-08-06 16:40:25 +0200

    feat: simple database connection (not tested)

---
+++ server/app/database.py
@@ -0,0 +1,16 @@
+import os
+
+import sqlalchemy
+import sqlalchemy.orm
+
+engine = sqlalchemy.create_engine(os.environ["DATABASE_URL"], echo=True)
+
+session_creator = sqlalchemy.orm.sessionmaker(bind=engine)
+
+
+def get_db():
+    db = session_creator()
+    try:
+        yield db
+    finally:
+        db.close()
